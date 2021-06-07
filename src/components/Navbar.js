76e2a9057ea7e499ed63b44642c807c1957dfc59
Parker Siddall <parker.siddall@gmail.com>
2021-06-07 22:54:58 +0200

    adding search to navbar, not yet functional

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { makeStyles } from '@material-ui/core/styles'
+import { fade, makeStyles } from '@material-ui/core/styles'
 import { useDispatch, useSelector } from 'react-redux'
 import clsx from 'clsx';
 
@@ -9,13 +9,18 @@ import { changeDrawerStatus } from '../reducers/drawerReducer'
 // components
 import AppBar from '@material-ui/core/AppBar'
 import IconButton from '@material-ui/core/IconButton'
+import InputBase from '@material-ui/core/InputBase'
 import MenuIcon from '@material-ui/icons/Menu'
+import SearchIcon from '@material-ui/icons/Search'
 import Toolbar from '@material-ui/core/Toolbar'
 import Typography from '@material-ui/core/Typography'
 
 const drawerWidth = 240
 
 const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
   customNavBar: {
     background: '#262626'
   },
@@ -35,7 +40,47 @@ const useStyles = makeStyles((theme) => ({
   },
   hide: {
     display: 'none',
-  }
+  },
+  search: {
+    position: 'fixed',
+    right: '20px',
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: fade(theme.palette.common.white, 0.15),
+    '&:hover': {
+      backgroundColor: fade(theme.palette.common.white, 0.25),
+    },
+    marginLeft: 0,
+    width: '100%',
+    [theme.breakpoints.up('xs')]: {
+      marginLeft: theme.spacing(1),
+      width: 'auto',
+    },
+  },
+  searchIcon: {
+    padding: theme.spacing(0, 2),
+    height: '100%',
+    position: 'absolute',
+    pointerEvents: 'none',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  inputRoot: {
+    color: 'inherit',
+  },
+  inputInput: {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
+    transition: theme.transitions.create('width'),
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      width: '12ch',
+      '&:focus': {
+        width: '20ch',
+      },
+    },
+  },
 }))
 
 const Navbar = () => {
@@ -45,7 +90,7 @@ const Navbar = () => {
   const dispatch = useDispatch()
 
   return(
-    <div>
+    <div className={classes.root}>
       <AppBar
         position='fixed'
         className={clsx(classes.appBar, {
@@ -65,6 +110,19 @@ const Navbar = () => {
           <Typography variant="h4" className={classes.navBarTitle}>
             Bookmarks
           </Typography>
+          <div className={classes.search}>
+            <div className={classes.searchIcon}>
+              <SearchIcon />
+            </div>
+            <InputBase
+              placeholder="Search…"
+              classes={{
+                root: classes.inputRoot,
+                input: classes.inputInput,
+              }}
+              inputProps={{ 'aria-label': 'search' }}
+            />
+          </div>
         </Toolbar>
       </AppBar>
     </div>
