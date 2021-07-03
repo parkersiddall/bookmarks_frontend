ff1cd9eee623e4718169623c633379d9bcc8fafd
Parker Siddall <parker.siddall@gmail.com>
2021-07-03 11:12:57 +0200

    Added collapse to show notes and pop up menu in bookmark card

--- src/components/BookmarkCard.js
+++ src/components/BookmarkCard.js
@@ -1,5 +1,7 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useDispatch } from 'react-redux'
+import { makeStyles } from '@material-ui/core/styles'
+import clsx from 'clsx'
 
 // reducers
 import { setCategory } from '../reducers/categoryReducer'
@@ -10,18 +12,52 @@ import CardActions from '@material-ui/core/CardActions'
 import CardActionArea from '@material-ui/core/CardActionArea'
 import CardContent from '@material-ui/core/CardContent'
 import CardMedia from '@material-ui/core/CardMedia'
+import Collapse from '@material-ui/core/Collapse'
+import Divider from '@material-ui/core/Divider'
 import FavoriteButton from './FavoriteButton'
 import IconButton from '@material-ui/core/IconButton'
 import InfoIcon from '@material-ui/icons/Info'
-import KeyboardArrowDownIcon from '@material-ui/icons/KeyboardArrowDown';
+import Menu from '@material-ui/core/Menu'
+import MenuItem from '@material-ui/core/MenuItem'
+import KeyboardArrowDownIcon from '@material-ui/icons/KeyboardArrowDown'
 import Link from '@material-ui/core/Link'
 import MoreVertIcon from '@material-ui/icons/MoreVert'
 import Tooltip from '@material-ui/core/Tooltip'
 import Typography from '@material-ui/core/Typography'
 
+const useStyles = makeStyles((theme) => ({
+    expand: {
+      transform: 'rotate(0deg)',
+      transition: theme.transitions.create('transform', {
+        duration: theme.transitions.duration.shortest,
+      }),
+    },
+    expandOpen: {
+      transform: 'rotate(180deg)',
+    }
+  }))
+
 const BookmarkCard = ({ bookmark }) => {
 
+    const classes = useStyles()
+
+    // local state to manage collapse and menu
+    const [expanded, setExpanded] = useState(false)
+    const [anchorEl, setAnchorEl] = React.useState(null)
     const dispatch = useDispatch()
+
+    // handlers for collapse and menu
+    const handleExpandClick = () => {
+      setExpanded(!expanded);
+    };
+
+    const handleClick = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
+
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
 
     return (
         <div>
@@ -37,7 +73,10 @@ const BookmarkCard = ({ bookmark }) => {
                     <Typography variant='h6'>
                         {bookmark.name}
                     </Typography>
-                    <Typography variant='subtitle1'>
+                    <Typography variant='subtitle2'
+                    gutterBottom
+                    >
+                        
                         <Link 
                             underline="none"
                             component="button"
@@ -47,21 +86,34 @@ const BookmarkCard = ({ bookmark }) => {
                             {bookmark.category}
                         </Link>
                     </Typography>
+                    <Collapse in={expanded} timeout="auto" unmountOnExit>
+                        <Typography variant='body2'>
+                            {bookmark.notes}
+                        </Typography>
+                    </Collapse>
                 </CardContent>
                 <CardActions 
                     disableSpacing
                 >
-                    <Tooltip title="Show notes">
-                        <IconButton 
-                            aria-label="Info">
-                            <KeyboardArrowDownIcon
-                                fontSize="small"
-                            />
-                        </IconButton>
-                    </Tooltip>
+                    { bookmark.notes && 
+                        <Tooltip title="Show notes">
+                            <IconButton 
+                                onClick={handleExpandClick}
+                                aria-label="Notes"
+                                className={clsx(classes.expand, {
+                                    [classes.expandOpen]: expanded,
+                                  })}
+                            >
+                                <KeyboardArrowDownIcon
+                                    fontSize="small"
+                                />
+                            </IconButton>
+                        </Tooltip>
+                    }
                     <IconButton 
+                        onClick={handleClick}
                         style={{marginLeft: 'auto'}}
-                        aria-label="Info">
+                        aria-label="Menu">
                         <MoreVertIcon
                             fontSize='small'
                         />
@@ -79,6 +131,18 @@ const BookmarkCard = ({ bookmark }) => {
                     />
                 </CardActions>
             </Card>
+            <Menu
+                id="simple-menu"
+                anchorEl={anchorEl}
+                keepMounted
+                open={Boolean(anchorEl)}
+                onClose={handleClose}
+            >
+                <MenuItem onClick={handleClose}>Edit Bookmark</MenuItem>
+                <MenuItem onClick={handleClose} color="secondary">Delete Bookmark</MenuItem>
+                <Divider />
+                <MenuItem onClick={handleClose}>Save Photo</MenuItem>
+            </Menu>
         </div>
     )
 }
