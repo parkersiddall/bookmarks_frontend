e32f8e696c1b2415b3a82405ecad4985ec105759
Parker Siddall <parker.siddall@gmail.com>
2021-07-08 18:01:19 +0200

    Styled login page

--- src/components/Login.js
+++ src/components/Login.js
@@ -7,24 +7,37 @@ import loginService from '../services/login'
 
 // components
 import Box from '@material-ui/core/Box'
+import Button from '@material-ui/core/Button'
 import Container from '@material-ui/core/Container'
+import Divider from '@material-ui/core/Divider'
 import Grid from '@material-ui/core/Grid'
+import TextField from '@material-ui/core/TextField'
+import { Typography } from '@material-ui/core'
+
+const prettyBackground = [
+  "https://i.imgur.com/4DA9jxT.jpg",
+  "https://i.redd.it/ggchwfr82q971.jpg",
+  "https://i.redd.it/g8w1nuzbon971.jpg",
+  "https://i.redd.it/jjq20pvhun971.jpg"
+]
+
+const randomElement = prettyBackground[Math.floor(Math.random() * prettyBackground.length)]
 
 const useStyles = makeStyles((theme) => ({
   pageRightContainer: {
     position: 'absolute',
     right: '0px',
     height: '100vh',
-    width: '30vw',
-    minWidth: '400px',
-    background: 'white'
+    width: '400px',
+    background: 'white',
+    boxShadow: "-10px 0px 100px black"
   },
   pageLeftContainer: {
     position: 'absolute',
-    right: '30vw',
+    right: '400px',
     height: '100vh',
     width: '100%',
-    background: 'url(https://i.redd.it/xrxwrj9e7l971.jpg)',
+    background:  `url(${randomElement})`,
     backgroundPosition: 'center',
     backgroundSize: 'cover'
 
@@ -67,41 +80,71 @@ const Login = () => {
   return(
     <div >
       <div className={classes.pageLeftContainer}>
-        sdfsd
       </div>
       <div className={classes.pageRightContainer}>
         <Container>
-          <form onSubmit={handleLogin}>
-          <div>
-            <input
-              label={'Username'}
-              id={'username'}
-              type={'text'}
-              value={username}
-              name={'username'}
-              onChange={({ target }) => setUsername(target.value)}
-            />
-          </div>
-          <div>
-            <input
-              label={'Password'}
-              id={'password'}
-              type={'password'}
-              value={password}
-              name={'password'}
-              onChange = {({ target }) => setPassword(target.value)}
-            />
-          </div>
-
-            <button
-            id='login-button'
-            variant="contained" 
-            color="primary" 
-            type="submit"
+          <Box my={5}>
+            <Typography variant={'h3'} color={'primary'}>
+              Bookmarke<span style={{color: 'black'}}>r/</span>
+            </Typography>
+            <Typography variant={'subtitle1'}>
+              With pretty pictures from your favorite subreddits
+            </Typography>
+          </Box>
+          <br/>
+          <Box mt={5} mb={2}>
+            <Typography variant={'h5'}>
+              <b>Log in to your account</b>
+            </Typography>
+            <br/>
+            <form onSubmit={handleLogin}>
+              <TextField
+                label={'Username'}
+                id={'username'}
+                type={'text'}
+                value={username}
+                name={'username'}
+                margin={'dense'}
+                variant={'outlined'}
+                fullWidth
+                required
+                onChange={({ target }) => setUsername(target.value)}
+              />
+              <TextField 
+                label={'Password'}
+                id={'password'}
+                type={'password'}
+                value={password}
+                name={'password'}
+                margin={'dense'}
+                variant={'outlined'}
+                fullWidth
+                required
+                onChange = {({ target }) => setPassword(target.value)}
+              />
+              <Box m={1}></Box>
+              <Button
+                id='login-button'
+                color='primary'
+                type='submit'
+                margin={'dense'}
+                variant={'contained'}
+                fullWidth
+              >
+                Login
+              </Button>
+            </form>
+          </Box>
+          <Divider />
+          <Box mt={2}>
+            <Button
+                  id='sign-up-button'
+                  fullWidth
             >
-              Login
-            </button>
-          </form>
+              Create an Account
+            </Button>
+
+          </Box>
         </Container>
       </div>
     </div>
